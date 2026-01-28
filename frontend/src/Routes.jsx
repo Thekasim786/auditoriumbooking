27c514d5fc3e353e380c33f61b74147eb48dfbc8
GatimSaini <133655718+GatimSaini@users.noreply.github.com>
2026-01-28 16:47:58 +0530

    Booking calendar view created

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -5,6 +5,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import ManagerDashboard from './pages/manager-dashboard';
 import FacultyDashboard from './pages/faculty-dashboard';
 import BookingRequestForm from './pages/booking-request-form';
+import BookingCalendarView from './pages/booking-calendar-view';
 import LoginPage from './pages/login-page';
 
 const Routes = () => {
@@ -17,6 +18,7 @@ const Routes = () => {
         <Route path="/manager-dashboard" element={<ManagerDashboard />} />
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/booking-request-form" element={<BookingRequestForm />} />
+        <Route path="/booking-calendar-view" element={<BookingCalendarView />} />
       </RouterRoutes>
       </ErrorBoundary>
     </BrowserRouter>
